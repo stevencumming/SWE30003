7ee074baf490ed0a8e39797ed586771f73a1cb90
stevencumming <101092147@student.swin.edu.au>
2022-05-22 18:05:41 +1000

    Initial Structure

---
+++ CozyKangaroo/CozyKangaroo/ApplicationFacade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CozyKangaroo
+{
+    class ApplicationFacade
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
