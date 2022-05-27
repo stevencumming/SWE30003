688bb4b9862d99a2a4907a05ede9d3ea9ebe4850
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-05-27 16:53:00 +1000

    Created WaitStaff scaffolding

--- CozyKangaroo/CozyKangaroo/WaitStaff.cs
+++ CozyKangaroo/CozyKangaroo/WaitStaff.cs
@@ -4,8 +4,44 @@ using System.Text;
 
 namespace CozyKangaroo
 {
-    class WaitStaff
+    class WaitStaff: Person
     {
+        private string username;
+        private string password;
 
+        public WaitStaff(string name, string username, string password) 
+        {
+            this.name = name;
+            this.username = username;
+            this.password = password;
+        }
+
+        public bool takeCustomerOrder(Customer customer) 
+        {
+            Order order = new Order();
+            Menu menu = new Menu();
+
+            return true;
+        }
+
+        public bool editOrder(Order order)
+        {
+            return true;
+        }
+
+        public Order getOrder(int orderId) 
+        {
+            return new Order();
+        }
+
+        public bool giveMeal(Customer customer, Meal meal)
+        {
+            return customer.recieveMeal(meal);
+        }
+
+        public bool markOrderComplete(Order order)
+        {
+            return true;
+        }
     }
 }
