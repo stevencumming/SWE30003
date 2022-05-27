7b409c299c28a72b918d3ca6778ec559f974f03c
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-05-27 17:26:21 +1000

    Added reservation and tables to application facade

--- CozyKangaroo/CozyKangaroo/ApplicationFacade.cs
+++ CozyKangaroo/CozyKangaroo/ApplicationFacade.cs
@@ -1,35 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CozyKangaroo
 {
-    class ApplicationFacade
+    sealed class ApplicationFacade
     {
-        static void Main(string[] args)
+        private static ApplicationFacade singleton = null;
+        private Reservation reservation = new Reservation(new List<Table>
         {
-           
-
-
-          
-
+            new Table(1, 2),
+            new Table(2, 4),
+            new Table(3, 1)
+        });
+        
+        // Disabled default constructor
+        private ApplicationFacade() {}
+        
+        // Singleton instance of class
+        public static ApplicationFacade Singleton 
+        {
+            get 
+            {
+                if (singleton == null) {
+                    singleton = new ApplicationFacade();
+                }
+                return singleton;
+            }
         }
 
-        
+        static void Main(string[] args)
+        {
 
+        }
 
         static void Login()
         {
             Console.WriteLine("Enter user name");
 
             string luser = Console.ReadLine();
+        }
 
-            switch (luser)
+        public Reservation Reservation
+        {
+            get 
             {
-                "aaa:
-                    Console.WriteLine();
-                    break;
-
-                default:
-                    break;
+                return reservation;
             }
         }
     }
