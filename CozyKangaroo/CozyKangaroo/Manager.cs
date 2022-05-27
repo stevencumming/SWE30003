87768dfe7cdeb246e225d665c18f397b6edb86ce
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-05-27 16:52:04 +1000

    Created manager scaffolding

--- CozyKangaroo/CozyKangaroo/Manager.cs
+++ CozyKangaroo/CozyKangaroo/Manager.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace CozyKangaroo
 {
-    class Manager
+    class Manager: Person
     {
+        private string username;
+        private string password;
+
+        public Manager(string name, string username, string password)
+        {
+            this.name = name;
+            this.username = username;
+            this.password = password;
+        }
+
+        public bool generateReport() {
+            return true;
+        }
+
+        public string viewReport(int reportId) {
+            return "report text";
+        }
     }
 }
